29821983dc4bad704607fc94fa010ecba28af9fa
Avstel <it.nathan.demissie@gmail.com>
2025-10-19 15:16:40 +0300

    Create seed_db.py

---
+++ seed_db.py
@@ -0,0 +1,6 @@
+from crm.models import Customer, Product
+
+Customer.objects.create(name="Alice", email="alice@example.com", phone="+1234567890")
+Customer.objects.create(name="Bob", email="bob@example.com", phone="123-456-7890")
+Product.objects.create(name="Laptop", price=999.99, stock=10)
+Product.objects.create(name="Mouse", price=49.99, stock=50)
