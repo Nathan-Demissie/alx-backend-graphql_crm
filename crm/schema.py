eb22535651537988a83c0671e009899c57ead3f3
Avstel <it.nathan.demissie@gmail.com>
2025-11-02 09:58:49 +0300

    Update schema.py

--- crm/schema.py
+++ crm/schema.py
@@ -3,6 +3,28 @@ from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 from .models import Customer, Product, Order
 from .filters import CustomerFilter, ProductFilter, OrderFilter
+from crm.models import Product
+
+class UpdateLowStockProducts(graphene.Mutation):
+    updated_products = graphene.List(graphene.String)
+    message = graphene.String()
+
+    def mutate(self, info):
+        low_stock_products = Product.objects.filter(stock__lt=10)
+        updated_names = []
+
+        for product in low_stock_products:
+            product.stock += 10
+            product.save()
+            updated_names.append(f"{product.name} ({product.stock})")
+
+        return UpdateLowStockProducts(
+            updated_products=updated_names,
+            message="Low-stock products updated successfully."
+        )
+
+class Mutation(graphene.ObjectType):
+    update_low_stock_products = UpdateLowStockProducts.Field()
 
 class CustomerNode(DjangoObjectType):
     class Meta:
