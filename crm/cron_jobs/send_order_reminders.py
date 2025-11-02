804982eb2670ef1c7273d89c5736a1e92a9592f9
Avstel <it.nathan.demissie@gmail.com>
2025-11-02 09:49:27 +0300

    Update send_order_reminders.py

--- crm/cron_jobs/send_order_reminders.py
+++ crm/cron_jobs/send_order_reminders.py
@@ -1,28 +1,32 @@
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
 import datetime
-import requests
-import json
 
-# Define GraphQL query
-query = """
-query {
-  orders(orderDate_Gte: "%s") {
-    id
-    customer {
-      email
-    }
-  }
-}
-""" % (datetime.datetime.now() - datetime.timedelta(days=7)).strftime('%Y-%m-%d')
-
-# Send request to GraphQL endpoint
-response = requests.post(
-    "http://localhost:8000/graphql",
-    json={"query": query},
-    headers={"Content-Type": "application/json"}
+# Set up GraphQL transport
+transport = RequestsHTTPTransport(
+    url="http://localhost:8000/graphql",
+    verify=True,
+    retries=3,
 )
 
-# Parse response
-orders = response.json().get("data", {}).get("orders", [])
+client = Client(transport=transport, fetch_schema_from_transport=True)
+
+# Define query
+seven_days_ago = (datetime.datetime.now() - datetime.timedelta(days=7)).strftime('%Y-%m-%d')
+query = gql(f"""
+query {{
+  orders(orderDate_Gte: "{seven_days_ago}") {{
+    id
+    customer {{
+      email
+    }}
+  }}
+}}
+""")
+
+# Execute query
+result = client.execute(query)
+orders = result.get("orders", [])
 
 # Log results
 with open("/tmp/order_reminders_log.txt", "a") as log_file:
