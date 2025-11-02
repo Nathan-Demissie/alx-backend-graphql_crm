f81a81e8cb622a9896bb400c5888f66e050abaa4
Avstel <it.nathan.demissie@gmail.com>
2025-11-02 10:03:55 +0300

    Update settings.py

--- crm/settings.py
+++ crm/settings.py
@@ -7,3 +7,14 @@ INSTALLED_APPS = [
 CRONJOBS = [
     ('*/5 * * * *', 'crm.cron.log_crm_heartbeat'), 'crm.cron.update_low_stock'
 ]
+
+from celery.schedules import crontab
+
+CELERY_BEAT_SCHEDULE = {
+    'generate-crm-report': {
+        'task': 'crm.tasks.generate_crm_report',
+        'schedule': crontab(day_of_week='mon', hour=6, minute=0),
+    },
+}
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
